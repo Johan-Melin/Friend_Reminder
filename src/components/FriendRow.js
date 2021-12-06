f133a0ff686ccc7ee1d8ba849a513dd76658d9c3
Johan Melin <soldierheart@hotmail.com>
2021-12-06 17:28:03 +0100

    updating working

--- src/components/FriendRow.js
+++ src/components/FriendRow.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
-const FriendRow = ({item, updateDate, deleteBtn}) => {
+const FriendRow = ({item, updateData, deleteBtn}) => {
     //.toISOString().substr(0, 10)
     return (
-        <form>
-            <input type="text" name={"name"} placeholder="name" defaultValue={item.name} />
-            <select defaultValue={item.type}>
+        <form onSubmit={e => { e.preventDefault(); }}>
+            <input type="text" name="name" placeholder="name" defaultValue={item.name} onChange={e => updateData(e, item.id)} />
+            <select name="type" defaultValue={item.type} onChange={e => updateData(e, item.id)}>
                 <option value="met">Met in person</option>
                 <option value="phone">Phone call</option>
                 <option value="message">Messaged</option>
                 <option value="video">Video call</option>
                 <option value="game">Played game</option>
+                <option value="mail">Mailed</option>
                 <option value="other">Other</option>
             </select>
-            <input type="date" id="contactDate" name="contactDate" defaultValue={item.date} onChange={val => updateDate(val)} />
-            <button onClick={e => deleteBtn(e, item.id)}>x</button>
+            <input type="date" name="date" defaultValue={item.date} onChange={e => updateData(e, item.id)} />
+            <button type="button" name="delete" onClick={e => deleteBtn(e, item.id)}>x</button>
         </form>
     )
 }
--- src/components/FriendsLister.js
+++ src/components/FriendsLister.js
@@ -19,12 +19,18 @@ const FriendsLister = () => {
         return Date.now().toString(36) + Math.random().toString(36).substr(2);
     }
 
-    const updateDate = (val) => {
-        console.log(val)
+    const updateData = (e, val) => {
+        const index = list.findIndex(item => item.id === val); 
+        if (index !== -1){
+            let tempList = list.slice();
+            tempList[index][e.target.name] = e.target.value;
+            setList(tempList);
+        }
     }
 
     const deleteBtn = (e, val) => {
         e.preventDefault();
+        console.log(e.target.id)
         setList(list.filter(item => item.id !== val));
     }
 
@@ -32,7 +38,7 @@ const FriendsLister = () => {
         <>
             {list.length === 0 ? <h2>Add friends to start</h2> : null}
             {list.map((item, index) => {
-                return <FriendRow key={index} item={item} updateDate={updateDate} deleteBtn={deleteBtn} />
+                return <FriendRow key={index} item={item} updateData={updateData} deleteBtn={deleteBtn} />
             })}
             <button onClick={addFriend}>Add Friend</button>
         </>
